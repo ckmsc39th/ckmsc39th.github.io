bbc736c49e1bf0c8a4abdcc083e4aa4a7e1ac42a
bensonhsieh2006 <bensonhsieh2006@gmail.com>
2023-03-08 22:14:53 +0800

    IG link added

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,12 @@
             <li><a href="/categories" class="hover:text-white">Categories</a></li>
             <li><a href="/works" class="hover:text-white">Works</a></li>
             <li><a href="/schedule" class="hover:text-white">Schedule</a></li>
-            <li><a href="/contast" class="hover:text-white">Contact</a></li>
+            <li><a href="/contact" class="hover:text-white">Contact</a></li>
           </ul>
         </div>
         <div class="items-centers flex gap-4">
           <a href="https://www.youtube.com/@ckmsc39th_luminescence" target="_blank" rel="noopener noreferrer" class="hover:text-white">Youtube</a>
-          <a href="#" target="_blank" rel="noopener noreferrer" class="hover:text-white">Instagram</a>
+          <a href="https://instagram.com/luminescence_ckmsc39th_" target="_blank" rel="noopener noreferrer" class="hover:text-white">Instagram</a>
           <a href="#" class="hover:text-white">Login</a>
         </div>
       </nav>
--- src/components/About.vue
+++ src/components/About.vue
@@ -1,3 +1,3 @@
 <template>
-  
+  <h1>Randint Ultradian</h1>
 </template>
