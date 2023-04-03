c368cad5dd45f93816daa498e04fdb5cff0eea5b
randint <lancameb@hotmail.com>
2023-04-03 23:23:57 +0800

    Made the tabs look tastier

--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -9,19 +9,26 @@
     <p>Hi! This is the Categories Page</p>
   </div>
   <br/>
-  <div class="tabs">
-    <div class="tab" v-for="tab in tabs" :key="tab"
-        @click="activeTab = tab" :class="{ active: activeTab === tab }">
+  <div class="flex justify-start space-x-4">
+    <div
+      v-for="tab in tabs"
+      :key="tab"
+      @click="activeTab = tab"
+      :class="[
+        'py-2 px-4 rounded-lg font-medium text-gray-500 hover:text-blue-500',
+        activeTab === tab ? 'text-blue-500' : ''
+      ]"
+    >
       {{ tab }}
     </div>
-    <div class="tab-content">
-      <div v-if="activeTab === 'Math'">Mathematics content goes here</div>
-      <div v-if="activeTab === 'Physics'">Physics content goes here</div>
-      <div v-if="activeTab === 'Chemistry'">Chemistry content goes here</div>
-      <div v-if="activeTab === 'Biology'">Biology content goes here</div>
-      <div v-if="activeTab === 'Informatics'">Informatics content goes here</div>
-      <div v-if="activeTab === 'Earth Sciences'">Earth Sciences content goes here</div>
-    </div>
+  </div>
+  <div class="py-4">
+    <div v-if="activeTab === 'Math'">Mathematics content goes here</div>
+    <div v-if="activeTab === 'Physics'">Physics content goes here</div>
+    <div v-if="activeTab === 'Chemistry'">Chemistry content goes here</div>
+    <div v-if="activeTab === 'Biology'">Biology content goes here</div>
+    <div v-if="activeTab === 'Informatics'">Informatics content goes here</div>
+    <div v-if="activeTab === 'Earth Sciences'">Earth Sciences content goes here</div>
   </div>
 </template>
 
@@ -38,33 +45,7 @@
   font-style: italic;
   font-size: xx-large;
 }
-.tabs {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 30px;
-  border-bottom: 1px solid gray;
-}
-
-.tab {
-  padding: 10px;
-  margin: 5px;
-  border: 1px solid gray;
-  border-bottom: none;
-  border-radius: 5px 5px 0 0;
-  cursor: pointer;
-  user-select: none;
-}
-
-.active {
-  background-color: white;
-  border-color: white;
-}
-
-.tab-content {
-  border: 1px solid gray;
-  padding: 10px;
-  border-radius: 0 5px 5px 5px;
-  margin-top: 0;
+.text-blue-500 {
+  color: #22a7f0;
 }
 </style>
