48fa221f4565965644563a18e732ca33e4e91db8
bensonhsieh2006 <bensonhsieh2006@gmail.com>
2023-04-05 01:14:50 +0800

    background added

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template> 
   <div class="app">
-    <div>
+    <div class="navbar">
       <navbar></navbar>
       <router-view />
     </div>
@@ -21,9 +21,47 @@ console.log(isMobile())
 
 <style>
 .app{
+  position: relative;
+  z-index: 0;
   min-width: fit-content;
   min-height: 100vh;
   background-color: rgb(31 41 55);
+  background-image: url(./keyVisualImg.JPG);
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-blend-mode: overlay;
   color: white;
+}
+
+.app::after{
+  content: '';
+  position: absolute;
+  z-index: 1;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  opacity: 0.55;
+  background-image: url(./dots.PNG);
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-blend-mode: overlay;
+  animation: background-flicker 7s linear infinite;
+}
+
+@keyframes background-flicker {
+  0% {
+    opacity: 0.55;
+  }
+  50% {
+    opacity: 0.05;
+  }
+}
+
+.navbar{
+  position: relative;
+  z-index: 2;
 }
 </style>
