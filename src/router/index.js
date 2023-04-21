0f1702cb2601323d0959e4a04466e4085a176c62
Yu-Chi Lai <125177850+expect1ai@users.noreply.github.com>
2023-04-21 12:44:14 +0800

    router bug fix attempt

--- src/router/index.js
+++ src/router/index.js
@@ -77,7 +77,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 });
 
