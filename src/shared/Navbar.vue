cb37d5869671906090d63c255c904af6394cb8c7
bensonhsieh2006 <bensonhsieh2006@gmail.com>
2023-04-27 19:57:20 +0800

    update

--- src/shared/Navbar.vue
+++ src/shared/Navbar.vue
@@ -379,8 +379,8 @@ nav.navbar {
 
 .login-container {
   background-color: #fff;
-  width: 25%;
-  padding: 2.5%;
+  width: 280px;
+  padding: 30px;
   border-radius: 10px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
   text-align: center;
