eacd76ffeffd677c642c4bc3cca18f7109563fd6
bensonhsieh2006 <bensonhsieh2006@gmail.com>
2023-04-27 19:22:42 +0800

    minor bug fixed

--- src/shared/Navbar.vue
+++ src/shared/Navbar.vue
@@ -368,8 +368,8 @@ nav.navbar {
   position: fixed;
   top: 0;
   left: 0;
-  width: 90%;
-  height: 90%;
+  width: 100%;
+  height: 100%;
   background-color: rgba(0, 0, 0, 0.7);
   z-index: 9999;
   display: flex;
@@ -379,8 +379,8 @@ nav.navbar {
 
 .login-container {
   background-color: #fff;
-  width: 80%;
-  padding: 10%;
+  width: 25%;
+  padding: 2.5%;
   border-radius: 10px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
   text-align: center;
